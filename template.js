a1c27510df1b121604b4f4f0679373695a3dbfd0
sadok.mourad-mhiri <sadok.mourad-mhiri@proxym-it.com>
2021-02-11 11:54:31 +0100

    fixed template.js

--- template.js
+++ template.js
@@ -215,7 +215,7 @@ const Template = ({
               className: "kbnWelcomeText",
               "data-error-message": i18n("core.ui.welcomeErrorMessage", {
                 defaultMessage:
-                  "Elastic did not load properly. Check the server output for more information.",
+                  "Green Metrics did not load properly. Check the server output for more information.",
               }),
             },
             i18n("core.ui.welcomeMessage", {
